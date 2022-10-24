abe534df7f432a185013f8a3664a57f6a4d5574d
ammartian <ammarhakimii1208@gmail.com>
2022-10-24 22:35:02 +0800

    Top 100 liked and 1

---
+++ top 100 liked/1. Two Sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+// Brute force using Array
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+// One-pass hash table
+// var twoSum = function(nums, target) {
+//     if (nums.length === 2) return [0, 1];
+//     const len = nums.length;
+//     let map = {};
+//     for(let i = 0; i < len; i++) {
+//         let n = target - nums[i];
+//         let find = map[n];
+//         if(find !== undefined) return [find, i];
+//         else map[nums[i]] = i;
+//     }
+// };
+
