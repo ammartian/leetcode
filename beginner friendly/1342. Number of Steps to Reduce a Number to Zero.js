560da8cc894b7bd62387aef418c378226565da2c
ammartian <ammarhakimii1208@gmail.com>
2022-10-24 20:14:15 +0800

    1342

--- beginner friendly/1342. Number of Steps to Reduce a Number to Zero.js
+++ beginner friendly/1342. Number of Steps to Reduce a Number to Zero.js
@@ -16,7 +16,7 @@ var numberOfSteps = function (num) {
     return step;
 };
 
-// Time complexity = O(n) [big O event] because we need to loop n time to get our output
+// Time complexity = O(logn) [big O of log n] because the number of steps it takes depends on the amount of time we divide the value of our input
 // Spacce complexity = O(1) [constant] because do not create data structure proportional in size to our input
 
 // Link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/solutions/?languageTags=javascript
