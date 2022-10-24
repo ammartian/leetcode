dd3b16c56df5a78a69a5e7e1225abbacdd111de1
ammartian <ammarhakimii1208@gmail.com>
2022-10-24 21:07:26 +0800

    383

---
+++ beginner friendly/383. Ransom Note.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+    const map = {};
+    // for(let letter of magazine) {
+    //     if (!map[letter]) {
+    //         map[letter] = 0;
+    //     }
+    //     map[letter]++;
+    // }
+
+    for (const letter of magazine) {
+        map[letter] = map[letter] + 1 || 1;
+    }
+
+    for (let letter of ransomNote) {
+        if (!map[letter]) {
+            return false;
+        }
+        map[letter]--;
+    }
+    return true;
+};
